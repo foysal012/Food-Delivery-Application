32e0a7b187127983bc4daac3bd626c77d48e4e45
Md. Foysal Joarder <foysaljoarder012@gmail.com>
2023-10-11 09:27:12 +0600

    search er kaz 80% complete, 20% baki and product delete from cart page complete but akta issue ase, and Theme changer trial purpages but not fully inplemented, akta issue bad e baki sob mutamuti complete insha allah otaw fix korbo

--- lib/screen/search_page_screen.dart
+++ lib/screen/search_page_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:food_delivery_app/model/categorymodel/remen_model_class.dart';
 import 'package:food_delivery_app/provider/category_provider_class.dart';
 import 'package:food_delivery_app/provider/theme_changer_provider.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 
 class SearchPageScreen extends StatefulWidget {
    SearchPageScreen({Key? key,}) : super(key: key);
 
-  RemenModelClass? remenModelClass;
 
   @override
   State<SearchPageScreen> createState() => _SearchPageScreenState();
@@ -18,6 +19,8 @@ class _SearchPageScreenState extends State<SearchPageScreen> {
   List<RemenModelClass> remenModelClassList = RemenModelClass.categoryRemenList;
 
   List<RemenModelClass> searchList = [];
+
+  FocusNode focusNode = FocusNode();
 
   List<String> poularList = [
     "sapporo miso",
@@ -47,7 +50,7 @@ class _SearchPageScreenState extends State<SearchPageScreen> {
 
                 TextFormField(
                 controller: _searchController,
-
+                focusNode: focusNode,
                 onChanged: (valued){
                   setState(() {
                     remenModelClassList.forEach((element) =>
@@ -67,6 +70,24 @@ class _SearchPageScreenState extends State<SearchPageScreen> {
                     );
                   });
                 },
+
+                  onEditingComplete: (){
+                    remenModelClassList.forEach((element) =>
+                    element.name!.toLowerCase().contains(_searchController.text.toString().toLowerCase()) == true ?
+                    searchList.add(RemenModelClass(
+                      name: element.name,
+                      rating: element.rating,
+                      imgUrl: element.imgUrl,
+                      distance: element.distance,
+                      price: element.price,
+                    ))
+                        :
+                    {
+
+                    }
+
+                    );
+                  },
 
                 decoration: InputDecoration(
                   hintText: "Search...",
@@ -88,6 +109,16 @@ class _SearchPageScreenState extends State<SearchPageScreen> {
                     ),
                   ),
                   labelText: "Search Food",
+                  suffixIcon: IconButton(
+                      onPressed: (){
+                        focusNode.unfocus();
+                        //focusNode.nextFocus();
+                        setState(() {
+
+                        });
+                      },
+                      icon: Icon(Icons.close, color: Colors.red,)
+                  )
                 ),
               ),
 
@@ -99,40 +130,219 @@ class _SearchPageScreenState extends State<SearchPageScreen> {
             Container(
               height: 150,
               width: double.infinity,
-              color: Colors.grey,
+              color: Colors.grey[200],
+
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: MasonryGridView.count(
+                  itemCount: poularList.length,
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    itemBuilder: (context, index){
+                      return InkWell(
+                          onTap: (){
+
+                              _searchController.text = poularList[index].toLowerCase() ;
+                            setState(() {
+
+                            });
+                          },
+                          child: Text("${poularList[index]}",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.black54,
+                          ),
+                          ),
+                      );
+                }),
+              )
             ),
 
             SizedBox(height: 10,),
 
             Expanded(
-                child: ListView.separated(
-                    itemBuilder: (context, index){
-                      return Container(
-                        height: 100,
-                        width: double.infinity,
+                child:
+                //searchList.isNotEmpty ?
+                _searchController.text.toString() != null ?
+                ListView.separated(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index){
+                    return Stack(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          padding: EdgeInsets.only(
+                              top: 5,
+                              left: 5,
+                              right: 5
+                          ),
+                          //height: 80,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(25),
 
-                        decoration: BoxDecoration(
-                          color: Colors.pink,
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(
-                            width: 2,
-                            color: Colors.purple,
-                          )
-                        ),
-                        child: Column(
-                          children: [
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Center(child: Image(image: AssetImage("${searchList[index].imgUrl}"), height: 170,width: 170,)),
 
-                            Text("${searchList[index].name}"),
-                          ],
+                              SizedBox(height: 8,),
+
+                              Center(child: Text("${searchList[index].name}",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontSize: 20
+                                ),
+                              ),),
+
+                              SizedBox(height: 10,),
+
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+
+                                  Row(
+                                    children: [
+                                      Icon(Icons.star, color: Colors.orange,),
+
+                                      SizedBox(width: 2,),
+
+                                      Text("${searchList[index].rating}",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black38,
+                                            fontSize: 20
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+
+                                  Row(
+                                    children: [
+                                      Icon(Icons.location_on, color: Colors.pinkAccent,),
+
+                                      SizedBox(width: 2,),
+
+                                      Text("${searchList[index].distance}",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black38,
+                                            fontSize: 20
+                                        ),
+                                      ),
+                                    ],
+                                  )
+
+                                ],
+                              ),
+
+                              SizedBox(height: 10,),
+
+                              Text("${searchList[index].price}",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontSize: 22
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      );
-                    },
-                    separatorBuilder: (_, index){
-                      return SizedBox(height: 10,);
-                    },
-                    itemCount: searchList.length,
-                ),
-            ),
+
+                        Positioned(
+                            bottom:0,
+                            right:0,
+                            child: Consumer<CategoryProviderClass>(builder: (context, value, child){
+                              return InkWell(
+                                onTap:(){
+                                  // setState(() {
+                                  //   // selectedItem++;
+                                  //   // print(selectedItem);
+                                  //   // value.setItemList(index);
+                                  //   // print("${value.allDataList}");
+                                  //
+                                  //   if(addItem.contains(remenData[index])) {
+                                  //     addItem.remove(
+                                  //         RemenModelClass(
+                                  //             name: remenData[index].name,
+                                  //             price: remenData[index].price,
+                                  //             imgUrl: remenData[index].imgUrl,
+                                  //             distance: remenData[index].distance,
+                                  //             rating: remenData[index].rating
+                                  //         )
+                                  //     );
+                                  //   } else {
+                                  //     addItem.add(RemenModelClass(
+                                  //         name: remenData[index].name,
+                                  //         price: remenData[index].price,
+                                  //         imgUrl: remenData[index].imgUrl,
+                                  //         distance: remenData[index].distance,
+                                  //         rating: remenData[index].rating
+                                  //     ));
+                                  //   }
+                                  //   // addItem.add(RemenModelClass(
+                                  //   //   name: remenData[index].name,
+                                  //   //   price: remenData[index].price,
+                                  //   //   imgUrl: remenData[index].imgUrl,
+                                  //   //   distance: remenData[index].distance,
+                                  //   //   rating: remenData[index].rating
+                                  //   // ));
+                                  //
+                                  //   print("${addItem.length}");
+                                  //   print("${addItem.toString()}");
+                                  //
+                                  // });
+
+                                  value.allDataList.contains(searchList[index]) == false ?
+
+                                  value.setAddItemList(RemenModelClass(
+                                      imgUrl: searchList[index].imgUrl,
+                                      name: searchList[index].name,
+                                      rating: searchList[index].rating,
+                                      distance: searchList[index].distance,
+                                      price: searchList[index].price
+                                  )) :
+                                  {
+
+                                  }
+                                  ;
+
+                                  print(value.allDataList.length);
+                                  print(value.allDataList.toList());
+                                },
+                                child: Container(
+                                  height: 45,
+                                  width: 45,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(20),
+                                      bottomRight: Radius.circular(20),
+                                    ),
+                                    color: Colors.black,
+                                  ),
+                                  child: Icon(Icons.shopping_cart_outlined, color: Colors.white,),
+                                ),
+                              );
+                            },)
+                        )
+                      ],
+                    );
+                  },
+                  separatorBuilder: (_,index){
+                    return SizedBox(
+                      width: 10,
+                    );
+                  },
+                  itemCount: searchList.length,
+                  //itemCount: categoryProviderClass.allDataList.length,
+                ) :  SizedBox()
+            )
 
 
 
